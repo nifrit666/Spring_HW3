662fe16ef4940e4781914a39e2afbc0d8c505685
«Andrey_Istomin» <an.istomin@gmail.com>
2024-09-24 22:47:24 +0500

    init HW3 for Spring

---
+++ src/test/java/ru/geekbrains/ProjectHW3/ProjectHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.ProjectHW3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
